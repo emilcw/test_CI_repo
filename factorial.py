9b04f27b4d42fad31cd75ffb93deb8e0ed240200
Emil Wiman <emiwi425@student.liu.se>
2022-10-28 11:39:47 +0200

    Added new factorial and tests

--- factorial.py
+++ factorial.py
@@ -1,4 +1,12 @@
-# Implementation of factorial
+# Python 3 program to find
+# factorial of given number
+import math
+
+# math's implementation of factorial, probably better than mine!
+def factorial_steroids(n):
+    return(math.factorial(n))
+
+# My implementation of factorial
 def factorial(n):
     if n == 0:
         return 1
--- test_factorial.py
+++ test_factorial.py
@@ -1,10 +1,20 @@
 from factorial import *
 
 def test_main():
+
+    #Test my implementation
     assert factorial(0) == 1
     assert factorial(1) == 1
     assert factorial(2) == 2
     assert factorial(3) == 6
     assert factorial(4) == 24
     assert factorial(5) == 120
+
+    #Test math's implementation
+    assert factorial_steroids(0) == 1
+    assert factorial_steroids(1) == 1
+    assert factorial_steroids(2) == 2
+    assert factorial_steroids(3) == 6
+    assert factorial_steroids(4) == 24
+    assert factorial_steroids(5) == 120
     print("Passed all tests!")
