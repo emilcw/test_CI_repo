456b8ce49c1dad5913876eb8300260dcde2bbaa3
Emil Wiman <emiwi425@student.liu.se>
2022-10-28 10:22:09 +0200

    Added factorial and tests

---
+++ factorial.py
@@ -0,0 +1,7 @@
+# Implementation of factorial
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
---
+++ tests.py
@@ -0,0 +1,10 @@
+from factorial import *
+
+if __name__ == '__main__':
+    assert factorial(0) == 1
+    assert factorial(1) == 1
+    assert factorial(2) == 2
+    assert factorial(3) == 6
+    assert factorial(4) == 24
+    assert factorial(5) == 120
+    print("Passed all tests!")
