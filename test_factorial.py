995f90d1552f92342618d3bcece6015168482725
Emil Wiman <emiwi425@student.liu.se>
2022-10-28 10:50:14 +0200

    Fixed test file

---
+++ test_factorial.py
@@ -0,0 +1,10 @@
+from factorial import *
+
+def test_main():
+    assert factorial(0) == 1
+    assert factorial(1) == 1
+    assert factorial(2) == 2
+    assert factorial(3) == 6
+    assert factorial(4) == 24
+    assert factorial(5) == 120
+    print("Passed all tests!")
